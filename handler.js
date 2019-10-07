acc3549f0cad8e8568d17f0f920db6a8d33d1e76
Katie Bayes <katpoe@gmail.com>
2019-10-07 18:19:07 -0400

    add basic API fetch

--- handler.js
+++ handler.js
@@ -1,27 +1,27 @@
-'use strict';
+const fetch = require('node-fetch');
+const axios = require('axios');
 
-const storyUrl = "http://datamine.mta.info/mta_esi.php?key=<key>&feed_id=1";
+const wmataURL = 'https://api.wmata.com/Incidents.svc/json/Incidents';
 
-const iopipe = require('@iopipe/iopipe')({
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlNzU4ZmMwNC0zOWFjLTQxNjQtYWNhNS1iNjRmN2VlZGNmNDAiLCJqdGkiOiJlYTM5MDYyYy03YzU5LTRhNTMtYTllNS0zZTBlZjI2NjhlZjAiLCJpYXQiOjE1MDIyODczNDgsImlzcyI6Imh0dHBzOi8vaW9waXBlLmNvbSIsImF1ZCI6Imh0dHBzOi8vaW9waXBlLmNvbSxodHRwczovL21ldHJpY3MtYXBpLmlvcGlwZS5jb20vZXZlbnQvLGh0dHBzOi8vZ3JhcGhxbC5pb3BpcGUuY29tIn0.06nUoeOOxchMUYyB8WYIhmLw3IQCcAb1GtB-12p_v9E'
-});
-
-module.exports.hello = async event => {
-  return {
-    iopipe(event, context) {
-      // run your code here normally
-      context.succeed({
-        statusCode: 200,
-        body: JSON.stringify(
-          {
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            input: event,
-          },
-          null,
-          2,
-      )});
+module.exports.fetch = async (event, context) => {
+  const response = await axios({
+    method: 'GET',
+    url: wmataURL,
+    headers: {
+      //public demo key
+      api_key: 'e13626d03d8e4c03ac07f95541b3091b'
     }
-  };
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  });
+
+  const incidents = response.data.Incidents;
+
+  const cleanIncidents = incidents.map(incident => {
+      return {
+        type: incident.IncidentType,
+        desc: incident.Description,
+        lines: incident.LinesAffected,
+        updated: incident.DateUpdated
+      }
+    });
+  return cleanIncidents;
 };
