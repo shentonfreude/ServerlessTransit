d593718e4570b08b9de10b4c935096d25a99ea31
Chris Shenton <chris@shenton.org>
2019-10-07 20:41:02 -0400

    Use super-undocumented DDB Deserializer to make python dict from ddb

--- handler.py
+++ handler.py
@@ -1,24 +1,51 @@
+import json
+import logging
+
+import boto3
+from boto3.dynamodb.types import TypeDeserializer
+
+
+LOG = logging.getLogger(__name__)
+
+
+def _ddb_deserialize(ddb_data):
+    return {k: TypeDeserializer().deserialize(v) for k, v in ddb_data.items()}
+
+
 def dynamodb_event(event, context):
     print(f'### event={event}')
     for rec in event['Records']:
-        ddb = rec['dynamodb']
-        # Somewhere there is a function which tranforms this raw DDB data to a
-        # Python Dict so I won't need this
-        keys = ddb['Keys']
-        pk = keys['pk']['S']
-        if not pk.startswith('report:'):
-            print('### Not a report, ignoring.')
-            return
-        # TODO: if it starts with "incident:" then call a functino to query the
-        # DB to find interested subscribers and send SNS.
-        sk = keys['sk']['S']
-        ni = ddb['NewImage']
-        transit_authority = ni['transit_authority']['S']
-        transit_line = ni['transit_line']['S']
-        transit_station = ni['transit_station']['S']
-        report_source = ni['report_source']['S']
-        report_text = ni['report_text']['S']
-        status = ni['status']['S']
-        print('### Got a report!')
-        # TODO: search DB for same line within last 30 minutes
-        # if found, create incident.
+        # TODO ensure INSERT as that's all we expect now
+        data = _ddb_deserialize(rec['dynamodb']['NewImage'])
+        print(f'### DATA={data}')
+        pk = data['pk']
+        if pk.startswith('report:'):
+            _report(data)
+        elif pk.startwith('incident:'):
+            _incident(data)
+        elif pk.startswith('subscriber:'):
+            _subscriber(data)
+        else:
+            LOG.error(f'unrecognized pk={pk} data={data}')
+
+def _report(data):
+    """search DB for same line within last 30 minutes if found, create incident record."""
+    print(f'### Got a report! data={data}')
+    # TODO: search DB for same line within last 30 minutes
+    # if found, create incident.
+
+def _incident(data):
+    """Call a functino to query the DB to find interested subscribers and send SNS."""
+    print(f'### Not yet processing incident data.')
+
+def _subscriber(data):
+    # do we ever want to process subscribers?
+    print(f'### Not yet processing subscriber data.')
+
+
+
+
+if __name__ == '__main__':
+    """Silly way to test."""
+    event = json.loads(open('event-report.json').read())
+    dynamodb_event(event, None)
